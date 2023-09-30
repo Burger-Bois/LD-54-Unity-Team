6a82bd36b9f961813d3429e995b7ce61c1ac09a0
PorgiesPie <melonmuncher1007@gmail.com>
2023-09-30 02:58:37 +0100

    j

--- Assets/Scripts/Spawner/SpawnManager.cs
+++ Assets/Scripts/Spawner/SpawnManager.cs
@@ -42,6 +42,7 @@ public class SpawnManager : MonoBehaviour
 
     private void Start()
     {
+        
         Random rand = new Random();
 
         var spawnPos = spawnPoints[rand.Next(0, 10)].transform.position;
